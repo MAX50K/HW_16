c9fe2bd1e9725a386b1c823a6239247073320e3d
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-05-23 18:22:09 +0200

    ses

--- js/task4.js
+++ js/task4.js
@@ -1,8 +1,25 @@
 class StringBuilder {
-    constructor(_value) {
-        this._value = _value;
-    }
+  constructor(value) {
+    this._value = value;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    this._value += str;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this._value = str + this._value + str;
+  }
 }
+
 const builder = new StringBuilder('.');
 
 builder.append('^');
